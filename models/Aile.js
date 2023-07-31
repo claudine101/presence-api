fc8ac00e95c67283c5fb1ef9e263125b85385370
vanny <vdevelopper@gmail.com>
2023-07-31 10:20:40 +0300

    suppressions

---
+++ models/Aile.js
@@ -0,0 +1,36 @@
+
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../utils/sequelize');
+
+/**
+* fonction model pour la creation de la table aile
+* @author Vanny Boy <vanny@mediabox.bi>
+* @date 31/07/2023
+* @returns 
+*/
+const Aile = sequelize.define("aile", {
+        ID_AILE: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true
+        },
+        ID_BATIMENT: {
+                type: Sequelize.INTEGER(),
+                allowNull: false
+        },
+        NUMERO_AILE: {
+                type: Sequelize.INTEGER(),
+                allowNull: true
+        },
+        DATE_INSERTION: {
+                type: DataTypes.DATE,
+                allowNull: false,
+                defaultValue: DataTypes.NOW
+        }
+}, {
+        freezeTableName: true,
+        tableName: 'aile',
+        timestamps: false,
+})
+module.exports = Volume
