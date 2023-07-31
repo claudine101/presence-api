3fca23abae309894bddc72b8f8c34f4f87bb37cd
vanny <vdevelopper@gmail.com>
2023-07-31 11:07:27 +0300

    feat(elements):creation de constants pour les etapes du folios

---
+++ constants/ETAPES_FOLIO.js
@@ -0,0 +1,15 @@
+
+/**
+* constantes pour le etapes du folio
+* @author Vanny Boy <vanny@mediabox.bi>
+* @date 31/07/2023
+*/
+
+const IDS_ETAPES_FOLIO = {
+        /**
+         * 1 - En attente de paiement
+         */
+        ETTENTE_PAIEMENET: 1
+}
+
+module.exports =  IDS_ETAPES_FOLIO
