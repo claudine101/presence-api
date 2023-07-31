836efc90134d46c2236486581bfac2eb98cb2464
vanny <vdevelopper@gmail.com>
2023-07-31 10:20:19 +0300

    suppressions

--- models/ExecQuery.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-const sequelize=require("../utils/sequelize")
-const readRequete =async(requete,ID_AILE)=>{
-    return await sequelize.query(requete, [ID_AILE])
-    // return query("SELECT * FROM remettants WHERE 1 AND ID_REMETTANT=?", [id]);
-}
-module.exports={
-    readRequete
-}
