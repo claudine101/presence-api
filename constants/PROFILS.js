f2add34956a1a62b730c61ec090f33ddc15a8814
eloge257 <niremaelog@gmail.com>
2023-07-31 10:23:14 +0200

    feat(constants):creation des constants

--- constants/PROFILS.js
+++ constants/PROFILS.js
@@ -49,6 +49,56 @@ const PROFILS = {
       * 10 - Chef d'equipe scanning (phase scanning)
       */
     CHEF_EQUIPE_SCANNING :10,
+    /**
+         * 11-Agent superviseur aile (phase scanning)
+         * 
+         */
+    AGENT_SUPERVISEUR_AILE_SCANNING: 11,
+    /**
+    * 12-Chef plateau (phase scanning)
+    * 
+    */
+    CHEF_PLATEAU: 12,
+    /**
+    * 13-Agent superviseur (phase scanning)
+    * 
+    */
+    AGENT_SUPERVISEUR: 13,
+    /**
+     * 14-Chef d'equipe (phase indexation)
+     * 
+     */
+    CHEF_EQUIPE_INDEXATION: 14,
+    /**
+     * 15-Agent superviseur aile (phase indexation)
+     * 
+     */
+    AGENT_SUPERVISEUR_AILE_INDEXATION: 15,
+    /**
+* 16-Chef Plateau (phase Indexation)
+* 
+*/
+    CHEF_PLATEAU_INDEXATION: 16,
+    /**
+* 17-Agent indexation (phase indexation)
+* 
+*/
+    AGENT_INDEXATION: 17,
+    /**
+* 18-Chef d'equipe (phase upload EDRMS)
+* 
+*/
+    CHEF_EQUIPE_PHASE_UPLOAD: 18,
+    /**
+* 19-Agent upload EDRMS (phase upload EDRMS)
+* 
+*/
+    AGENT_UPLOAD_EDRMS: 19,
+    /**
+* 20-Verificateur upload EDRMS (phase upload EDRMS)
+* 
+*/
+    VERIFICATEUR_UPLOAD: 20,
 }
 
 module.exports = PROFILS
