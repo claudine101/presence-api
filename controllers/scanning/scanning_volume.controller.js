bed66912b955830248ece3a7770753fb62846fd1
vanny <vdevelopper@gmail.com>
2023-08-01 13:12:12 +0300

    feat(elements):finalisation de routes de scanning pour les formulaires

--- controllers/scanning/scanning_volume.controller.js
+++ controllers/scanning/scanning_volume.controller.js
@@ -10,9 +10,12 @@ const Validation = require('../../class/Validation');
 const IMAGES_DESTINATIONS = require('../../constants/IMAGES_DESTINATIONS');
 const { excludedProperties } = require('juice');
 const DossiersUpload = require('../../class/uploads/DossiersUpload');
+const ETAPES_VOLUME = require('../../constants/ETAPES_VOLUME');
+const ETAPES_FOLIO = require('../../constants/ETAPES_FOLIO');
 const Volume = require('../../models/Volume');
 const Etapes_volume_historiques = require('../../models/Etapes_volume_historiques');
 const Folio = require('../../models/Folio');
+const Etapes_folio_historiques = require('../../models/Etapes_folio_historiques');
 
 /**
  * Permet de faire la mise a jour des volume envoyer entre un agent superviseur aille phase scanning
@@ -26,7 +29,7 @@ const Folio = require('../../models/Folio');
 const volumeScanning = async (req, res) => {
     try {
         const { ID_VOLUME } = req.params
-        const { ID_ETAPE_VOLUME, USER_TRAITEMENT } = req.body
+        const { USER_TRAITEMENT} = req.body
         const validation = new Validation(
             { ...req.body, ...req.files },
             {
@@ -34,7 +37,7 @@ const volumeScanning = async (req, res) => {
                     required: true,
                     image: 21000000
                 },
-                ID_ETAPE_VOLUME: {
+                USER_TRAITEMENT: {
                     required: true,
                 }
             },
@@ -43,7 +46,7 @@ const volumeScanning = async (req, res) => {
                     image: "La taille invalide",
                     required: "Le nom est obligatoire"
                 },
-                ID_ETAPE_VOLUME: {
+                USER_TRAITEMENT: {
                     required: "ID_ETAPE_VOLUME est obligatoire",
                 }
             }
@@ -68,7 +71,7 @@ const volumeScanning = async (req, res) => {
         }
 
         const results = await Volume.update({
-            ID_ETAPE_VOLUME: ID_ETAPE_VOLUME
+            ID_ETAPE_VOLUME: ETAPES_VOLUME.SELECTION_CHEF_EQUIPE_SCANNING
         }, {
             where: {
                 ID_VOLUME: ID_VOLUME
@@ -78,7 +81,7 @@ const volumeScanning = async (req, res) => {
             USERS_ID: req.userId,
             USER_TRAITEMENT: USER_TRAITEMENT,
             ID_VOLUME: ID_VOLUME,
-            ID_ETAPE_VOLUME: ID_ETAPE_VOLUME,
+            ID_ETAPE_VOLUME: ETAPES_VOLUME.SELECTION_CHEF_EQUIPE_SCANNING,
             PV_PATH: filename_pv ? `${req.protocol}://${req.get("host")}${IMAGES_DESTINATIONS.pv}/${filename_pv.fileName}` : null,
         })
         res.status(RESPONSE_CODES.CREATED).json({
@@ -108,7 +111,7 @@ const volumeScanning = async (req, res) => {
 const volumeAileScanning = async (req, res) => {
     try {
         const { ID_VOLUME } = req.params
-        const { ID_ETAPE_VOLUME, USER_TRAITEMENT } = req.body
+        const {USER_TRAITEMENT} = req.body
         const validation = new Validation(
             { ...req.body, ...req.files },
             {
@@ -116,7 +119,7 @@ const volumeAileScanning = async (req, res) => {
                     required: true,
                     image: 21000000
                 },
-                ID_ETAPE_VOLUME: {
+                USER_TRAITEMENT: {
                     required: true,
                 }
             },
@@ -125,7 +128,7 @@ const volumeAileScanning = async (req, res) => {
                     image: "La taille invalide",
                     required: "Le nom est obligatoire"
                 },
-                ID_ETAPE_VOLUME: {
+                USER_TRAITEMENT: {
                     required: "ID_ETAPE_VOLUME est obligatoire",
                 }
             }
@@ -150,7 +153,7 @@ const volumeAileScanning = async (req, res) => {
         }
 
         const results = await Volume.update({
-            ID_ETAPE_VOLUME: ID_ETAPE_VOLUME
+            ID_ETAPE_VOLUME:ETAPES_VOLUME.SELECTION_AGENT_SUP_AILE_SCANNING_FOLIO_TRAITES
         }, {
             where: {
                 ID_VOLUME: ID_VOLUME
@@ -160,7 +163,7 @@ const volumeAileScanning = async (req, res) => {
             USERS_ID: req.userId,
             USER_TRAITEMENT: USER_TRAITEMENT,
             ID_VOLUME: ID_VOLUME,
-            ID_ETAPE_VOLUME: ID_ETAPE_VOLUME,
+            ID_ETAPE_VOLUME: ETAPES_VOLUME.SELECTION_AGENT_SUP_AILE_SCANNING_FOLIO_TRAITES,
             PV_PATH: filename_pv ? `${req.protocol}://${req.get("host")}${IMAGES_DESTINATIONS.pv}/${filename_pv.fileName}` : null,
         })
         res.status(RESPONSE_CODES.CREATED).json({
@@ -179,7 +182,7 @@ const volumeAileScanning = async (req, res) => {
 }
 
 /**
- * Permet de faire la mise a jour des folios agent a un agent superviseur scanning
+ * Permet de faire la mise a jour des folios qu'un chef pleteau donnent a un agent superviseur scanning
  * @author Vanny Boy <vanny@mediabox.bi>
  * @param {express.Request} req
  * @param {express.Response} res 
@@ -192,7 +195,6 @@ const folioChefScanning = async (req, res) => {
         const {
             ID_VOLUME,
             folio,
-            ID_ETAPE_VOLUME, 
             USER_TRAITEMENT
         } = req.body;
         const PV = req.files?.PV
@@ -202,12 +204,18 @@ const folioChefScanning = async (req, res) => {
                 PV: {
                     required: true,
                     image: 21000000
+                },
+                USER_TRAITEMENT: {
+                    required: true,
                 }
             },
             {
                 PV: {
                     image: "La taille invalide",
                     required: "Le nom est obligatoire"
+                },
+                USER_TRAITEMENT: {
+                    required: "ID_ETAPE_VOLUME est obligatoire",
                 }
             }
         );
@@ -234,36 +242,123 @@ const folioChefScanning = async (req, res) => {
         await Promise.all(folioObjet.map(async (folio) => {
             // const CODE_REFERENCE = `${folio.NUMERO_FOLIO}${req.userId}${moment().get("s")}`
             const dateinsert = moment(new Date()).format("YYYY-MM-DD HH:mm:ss")
-            await Folio.create(
+            await Folio.update(
                 {
+                    ID_ETAPE_FOLIO: ETAPES_FOLIO.SELECTION_AGENT_SUP_SCANNIMG
+                }, {
+                where: {
                     ID_VOLUME: ID_VOLUME,
                     ID_NATURE: folio.ID_NATURE,
-                    NUMERO_FOLIO: folio.NUMERO_FOLIO,
-                    CODE_FOLIO: CODE_REFERENCE,
-                    NUMERO_DOSSIERS: folio.NUMERO_DOSSIERS,
-                    ID_USERS: req.userId,
-                    ID_FOLIO_PV: histoPv.ID_FOLIO_PV,
-                    ID_ETAPE_FOLIO: 1
+                    NUMERO_FOLIO: folio.NUMERO_FOLIO
                 }
-            )
-        }))
-        const results = await Volume.update({
-            ID_ETAPE_VOLUME: 3,
-        }, {
-            where: {
-                ID_VOLUME: ID_VOLUME
             }
-        })
-        await Etapes_volume_historiques.create({
-            USERS_ID: req.userId,
-            USER_TRAITEMENT: req.userId,
-            ID_VOLUME: ID_VOLUME,
-            ID_ETAPE_VOLUME: 3
-        })
+            )
+            await Etapes_folio_historiques.create({
+                ID_USER: req.userId,
+                USER_TRAITEMENT: USER_TRAITEMENT,
+                ID_FOLIO: folio.ID_FOLIO,
+                ID_ETAPE_FOLIO: ETAPES_FOLIO.SELECTION_AGENT_SUP_SCANNIMG,
+                PV_PATH: filename_pv ? `${req.protocol}://${req.get("host")}${IMAGES_DESTINATIONS.pv}/${filename_pv.fileName}` : null,
+            })
+        }))
         res.status(RESPONSE_CODES.CREATED).json({
             statusCode: RESPONSE_CODES.CREATED,
             httpStatus: RESPONSE_STATUS.CREATED,
-            message: "Insertion faite  avec succès",
+            message: "modification faite  avec succès",
+            // result: reponse
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(RESPONSE_CODES.INTERNAL_SERVER_ERROR).json({
+            statusCode: RESPONSE_CODES.INTERNAL_SERVER_ERROR,
+            httpStatus: RESPONSE_STATUS.INTERNAL_SERVER_ERROR,
+            message: "Erreur interne du serveur, réessayer plus tard",
+        })
+    }
+}
+
+/**
+ * Permet de faire la mise a jour des folios qu'un agent superviseur scanning donnent a une equipe scanning
+ * @author Vanny Boy <vanny@mediabox.bi>
+ * @param {express.Request} req
+ * @param {express.Response} res 
+ * @date  31/07/2023
+ * 
+ */
+
+const folioSupScanning = async (req, res) => {
+    try {
+        const {
+            ID_VOLUME,
+            folio,
+            USER_TRAITEMENT
+        } = req.body;
+        const PV = req.files?.PV
+        const validation = new Validation(
+            { ...req.body, ...req.files },
+            {
+                PV: {
+                    required: true,
+                    image: 21000000
+                },
+                USER_TRAITEMENT: {
+                    required: true,
+                }
+            },
+            {
+                PV: {
+                    image: "La taille invalide",
+                    required: "Le nom est obligatoire"
+                },
+                USER_TRAITEMENT: {
+                    required: "ID_ETAPE_VOLUME est obligatoire",
+                }
+            }
+        );
+        await validation.run();
+        const isValid = await validation.isValidate()
+        const errors = await validation.getErrors()
+        if (!isValid) {
+            return res.status(RESPONSE_CODES.UNPROCESSABLE_ENTITY).json({
+                statusCode: RESPONSE_CODES.UNPROCESSABLE_ENTITY,
+                httpStatus: RESPONSE_STATUS.UNPROCESSABLE_ENTITY,
+                message: "Probleme de validation des donnees",
+                result: errors
+            })
+        }
+        const volumeUpload = new VolumePvUpload()
+        var filename_pv
+        if (PV) {
+            const { fileInfo: fileInfo_2, thumbInfo: thumbInfo_2 } = await volumeUpload.upload(PV, false)
+            filename_pv = fileInfo_2
+        }
+        var folioObjet = {}
+        folioObjet = JSON.parse(folio)
+        // folioObjet = folio
+        await Promise.all(folioObjet.map(async (folio) => {
+            await Folio.update(
+                {
+                    ID_ETAPE_FOLIO: ETAPES_FOLIO.SELECTION_EQUIPE_SCANNIMG
+                }, {
+                where: {
+                    ID_VOLUME: ID_VOLUME,
+                    ID_NATURE: folio.ID_NATURE,
+                    NUMERO_FOLIO: folio.NUMERO_FOLIO
+                }
+            }
+            )
+            await Etapes_folio_historiques.create({
+                ID_USER: req.userId,
+                USER_TRAITEMENT: USER_TRAITEMENT,
+                ID_FOLIO: folio.ID_FOLIO,
+                ID_ETAPE_FOLIO: ETAPES_FOLIO.SELECTION_EQUIPE_SCANNIMG,
+                PV_PATH: filename_pv ? `${req.protocol}://${req.get("host")}${IMAGES_DESTINATIONS.pv}/${filename_pv.fileName}` : null,
+            })
+        }))
+        res.status(RESPONSE_CODES.CREATED).json({
+            statusCode: RESPONSE_CODES.CREATED,
+            httpStatus: RESPONSE_STATUS.CREATED,
+            message: "modification faite  avec succès",
             // result: reponse
         })
     } catch (error) {
@@ -279,5 +374,6 @@ const folioChefScanning = async (req, res) => {
 module.exports = {
     volumeScanning,
     volumeAileScanning,
-    folioChefScanning
+    folioChefScanning,
+    folioSupScanning
 }
--- routes/scanning/scanning_volume.routes.js
+++ routes/scanning/scanning_volume.routes.js
@@ -7,20 +7,27 @@ const scanning_volume_routes = express.Router()
  *@method PUT
  * @url /scanning/volume
  */
- scanning_volume_routes.put('/:ID_VOLUME', scanning_volume_controller.volumeScanning)
+scanning_volume_routes.put('/:ID_VOLUME', scanning_volume_controller.volumeScanning)
 
- /**
- * Une route pour envoyer le volumes chef un agent de plateau
- *@method PUT
- * @url /scanning/volume/aile
- */
- scanning_volume_routes.put('/aile/:ID_VOLUME', scanning_volume_controller.volumeAileScanning)
+/**
+* Une route pour envoyer le volumes chef un agent de plateau
+*@method PUT
+* @url /scanning/volume/aile
+*/
+scanning_volume_routes.put('/aile/:ID_VOLUME', scanning_volume_controller.volumeAileScanning)
 
-  /**
- * Une route pour envoyer les folios du chef plateau vers un agent superviseur scanning
- *@method PUT
- * @url /scanning/volume/folio/plateau
- */
- scanning_volume_routes.put('/folio/plateau', scanning_volume_controller.folioChefScanning)
+/**
+* Une route pour envoyer les folios du chef plateau vers un agent superviseur scanning
+*@method PUT
+* @url /scanning/volume/folio/plateau
+*/
+scanning_volume_routes.put('/folio/plateau', scanning_volume_controller.folioChefScanning)
+
+/**
+* Une route pour envoyer les folios d'un agent superviseur scanning vers equipe scanning
+*@method PUT
+* @url /scanning/volume/folio/equipe
+*/
+scanning_volume_routes.put('/folio/equipe', scanning_volume_controller.folioSupScanning)
 
 module.exports = scanning_volume_routes
