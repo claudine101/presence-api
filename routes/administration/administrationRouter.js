378387c7ac8491be7e218c4054bf2fa893c19885
deco257 <derickkazikazi@gmail.comgit config --global user.email derickkazikazi@gmail.comgit config --global user.email derickkazikazi@gmail.com>
2023-08-03 12:15:08 -0100

    amelioration

--- routes/administration/administrationRouter.js
+++ routes/administration/administrationRouter.js
@@ -7,6 +7,8 @@ const equipe_routes= require('./equipe_adminstration.routes')
 const etape_folio_routes= require('./etape_folio_administration.routes')
 const phase_routes= require('./Phase_administration.routes')
 const volume_folio_routes =require('./volume_folio_administration.routes')
+const malle_routes =require('./malle_administration.routes')
+const aile__routes= require('./aile_administration.routes')
 
 const administrationRouter=express.Router();
 
@@ -17,5 +19,7 @@ administrationRouter.use('/equipe',equipe_routes)
 administrationRouter.use('/etape_folio',etape_folio_routes)
 administrationRouter.use('/phase',phase_routes)
 administrationRouter.use('/volume_folio',volume_folio_routes)
+administrationRouter.use('/malle',malle_routes)
+administrationRouter.use('/aille',aile__routes)
 
 module.exports = administrationRouter
