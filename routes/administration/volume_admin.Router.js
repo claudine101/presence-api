a8259eb8e67422ca6a56a5f7783fb7faf2342b3a
eloge257 <niremaelog@gmail.com>
2023-08-03 09:07:08 +0200

    detail volume

--- routes/administration/volume_admin.Router.js
+++ routes/administration/volume_admin.Router.js
@@ -18,6 +18,13 @@ const volumes_routes = express.Router()
  */
  volumes_routes.get("/HistoVolume/:ID_VOLUME",volume_routes_controller.getHistoriqueVolume);
 
+   /**
+ * Une route pour la detail des folio  
+ *@method GET
+ * @url /admin/Agent_folio/
+ */
+ volumes_routes.get("/Agent_folio/:ID_VOLUME",volume_routes_controller.getHistoriqueFolio);
+
  
 
 module.exports = volumes_routes
