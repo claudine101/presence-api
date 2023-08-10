ea9047d861c2be0bea4df7b1fe188bf49ca2def8
vanny <vdevelopper@gmail.com>
2023-08-10 13:05:47 +0300

    correction de retour chez un agent ailles

--- controllers/scanning/scanning_volume.controller.js
+++ controllers/scanning/scanning_volume.controller.js
@@ -1194,6 +1194,91 @@ const updateRetourPlateauSup = async (req, res) => {
  * @date  4/08/2023
  * 
  */
+
+// const findAllVolumerSupAille = async (req, res) => {
+//     try {
+//         const result = await Etapes_folio_historiques.findAll({
+//             where: {
+//                 [Op.and]: [{
+//                     ID_USER: req.userId,
+//                 }]
+//             },
+//             attributes: ['ID_FOLIO_HISTORIQUE', 'USER_TRAITEMENT', 'ID_ETAPE_FOLIO', 'DATE_INSERTION'],
+//             include: [
+//                 {
+//                     model: Folio,
+//                     as: 'folio',
+//                     required: true,
+//                     attributes: ['ID_FOLIO', 'ID_ETAPE_FOLIO', 'NUMERO_FOLIO', 'CODE_FOLIO'],
+//                     where: {
+//                         ID_ETAPE_FOLIO: {
+//                             [Op.in]: [
+//                                 ETAPES_FOLIO.SELECTION_AGENT_SUP_SCANNIMG,
+//                                 ETAPES_FOLIO.SELECTION_EQUIPE_SCANNIMG,
+//                                 ETAPES_FOLIO.RETOUR_EQUIPE_SCANNING_V_AGENT_SUP_SCANNING
+//                             ]
+//                         }
+//                     },
+//                     // include: [
+//                     //     {
+//                     //         model: Equipes,
+//                     //         as: 'equipe',
+//                     //         required: false,
+//                     //         attributes: ['ID_EQUIPE', 'NOM_EQUIPE', 'CHAINE', 'ORDINATEUR'],
+//                     //     }
+//                     // ]
+//                 },
+//                 {
+//                     model: Users,
+//                     as: 'traitement',
+//                     required: false,
+//                     attributes: ['USERS_ID', 'NOM', 'PRENOM', 'EMAIL'],
+//                 },
+//             ]
+//         })
+//         var UserFolios = []
+//         result.forEach(user => {
+//             const USERS_ID = user.traitement?.USERS_ID
+//             const users = user.traitement
+//             const isExists = UserFolios.find(vol => vol.USERS_ID == USERS_ID) ? true : false
+//             if (isExists) {
+//                 const volume = UserFolios.find(vol => vol.USERS_ID == USERS_ID)
+//                 const newVolumes = { ...volume, folios: [...volume.folios, user] }
+//                 UserFolios = UserFolios.map(vol => {
+//                     if (vol.USERS_ID == USERS_ID) {
+//                         return newVolumes
+//                     } else {
+//                         return vol
+//                     }
+//                 })
+//             } else {
+//                 UserFolios.push({
+//                     USERS_ID,
+//                     users,
+//                     folios: [user]
+//                 })
+
+//             }
+
+//         })
+//         res.status(RESPONSE_CODES.OK).json({
+//             statusCode: RESPONSE_CODES.OK,
+//             httpStatus: RESPONSE_STATUS.OK,
+//             message: "Liste des folio donnees",
+//             UserFolios
+//             // result:result
+//         })
+//     } catch (error) {
+//         console.log(error)
+//         res.status(RESPONSE_CODES.INTERNAL_SERVER_ERROR).json({
+//             statusCode: RESPONSE_CODES.INTERNAL_SERVER_ERROR,
+//             httpStatus: RESPONSE_STATUS.INTERNAL_SERVER_ERROR,
+//             message: "Erreur interne du serveur, réessayer plus tard",
+//         })
+//     }
+// }
+
+
 const findAllVolumerSupAille = async (req, res) => {
     try {
         const userObject = await Users.findOne({
