7936542aa084b8965465f24ada1e00168b666811
habiyakareleonard <habiyakareleonard2019@gmail.com>
2023-08-22 15:48:24 +0200

    rapport phase scanning

--- controllers/administration/rapport/phase_scanning.controller.js
+++ controllers/administration/rapport/phase_scanning.controller.js
@@ -2,7 +2,6 @@ const express = require("express")
 const RESPONSE_CODES = require('../../../constants/RESPONSE_CODES')
 const RESPONSE_STATUS = require("../../../constants/RESPONSE_STATUS")
 const Folio = require("../../../models/Folio")
-
 const { Op } = require("sequelize")
 const Equipes = require("../../../models/Equipes")
 
@@ -15,33 +14,90 @@ const Equipes = require("../../../models/Equipes")
  * @date 21/08/2023
  */
 
-const dossiers_scannes_non_scannees = async (req, res) => {
-  const { equipedata } = req.params;
+const phaseScanning = async (req, res) => {
+
   try {
-    const allPhasesCan = await Folio.findAll({
-      attributes: ['NUMERO_FOLIO', 'CODE_FOLIO', 'NUMERO_PARCELLE', 'LOCALITE', 'NOM_PROPRIETAIRE',
-        'PRENOM_PROPRIETAIRE', 'NOMBRE_DOUBLON', 'IS_RECONCILIE'],
+    const phase = await Folio.findAll({
       include: {
         model: Equipes,
         as: 'equipe',
-        required: false,
+        required: true,
         attributes: ['ID_EQUIPE', 'NOM_EQUIPE']
       },
-      where: {
-        ID_FOLIO_EQUIPE:equipedata || '',
-        // IS_RECONCILIE: {
-        //   [Op.in]: [0, 1,'NULL']
-        // }
-      }
     });
 
-    // console.log(equipedata)
+    //group by
+    const uniqueIds = [];
+    const phaserows = phase.filter(element => {
+      const isDuplicate = uniqueIds.includes(element.equipe.NOM_EQUIPE);
+      if (!isDuplicate) {
+        uniqueIds.push(element.equipe.NOM_EQUIPE);
+        return true;
+      }
+      return false;
+    });
+    const phasedata = phaserows
 
+    //find equipe
+    const equipe = phasedata.map((equip) => {
+      return equip.equipe.NOM_EQUIPE
+    });
+
+    //count dossiers scannees
+    const numberdossiersScannees = await Promise.all(phasedata.map(async countObject => {
+      const util = countObject.toJSON()
+      const fetCmpt = await Folio.count({
+        where: { ID_FOLIO_EQUIPE: util.ID_FOLIO_EQUIPE, IS_RECONCILIE: 1 }
+      })
+      return [fetCmpt]
+    }))
+
+    //count dossiers non scannees
+    const numberdossiers_non_scannees = await Promise.all(phasedata.map(async countObject => {
+      const util = countObject.toJSON()
+      const fetCmpt = await Folio.count({
+        where: { ID_FOLIO_EQUIPE: util.ID_FOLIO_EQUIPE, IS_RECONCILIE: 0 }
+      })
+      return [fetCmpt]
+    }))
+
+    //count dossiers en cours de traitement
+    const numberdossiers_encours_traitement = await Promise.all(phasedata.map(async countObject => {
+      const util = countObject.toJSON()
+      const fetCmpt = await Folio.count({
+        where: { ID_FOLIO_EQUIPE: util.ID_FOLIO_EQUIPE, IS_RECONCILIE: null }
+      });
+      return [fetCmpt]
+    }))
+    //highchart
+    const options = {
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'Rapport de la phase scanning'
+      },
+      xAxis: {
+        categories: equipe
+      },
+      series: [{
+        name: 'Dossiers Scannés',
+        data: numberdossiersScannees
+      },
+      {
+        name: 'Dossiers non Scannés',
+        data: numberdossiers_non_scannees
+      }, {
+        name: 'Dossiers en attentes de traitement',
+        data: numberdossiers_encours_traitement
+      }
+      ]
+    }
     res.status(RESPONSE_CODES.OK).json({
       statusCode: RESPONSE_CODES.OK,
       httpStatus: RESPONSE_STATUS.OK,
       message: 'les dossiers',
-      result: allPhasesCan
+      result: options
     });
 
 
@@ -54,46 +110,7 @@ const dossiers_scannes_non_scannees = async (req, res) => {
     })
   }
 }
-
-
-
-/**
- * Fonction pour lister les equipes
- * @param {express.Request} req 
- * @param {express.Response} res 
- * @author leonard<leonard@mdiabox.bi>
- * @date 21/08/2023
- */
-
-const equipesAll = async (req, res) => {
-  try {
-
-    const equipe = await Equipes.findAll({
-      attributes: ['ID_EQUIPE', 'NOM_EQUIPE']
-    })
-    // console.log(equipe)
-
-    res.status(RESPONSE_CODES.OK).json({
-      statusCode: RESPONSE_CODES.OK,
-      httpStatus: RESPONSE_STATUS.OK,
-      message: 'les equipes',
-      result: equipe
-    });
-
-
-  } catch (error) {
-    console.log(error)
-    res.status(RESPONSE_CODES.INTERNAL_SERVER_ERROR).json({
-      statusCode: RESPONSE_CODES.INTERNAL_SERVER_ERROR,
-      httpStatus: RESPONSE_STATUS.INTERNAL_SERVER_ERROR,
-      message: "Erreur interne du serveur, réessayer plus tard",
-    })
-  }
-}
-
 
 module.exports = {
-  dossiers_scannes_non_scannees,
-  equipesAll
-
+  phaseScanning
 }
--- routes/administration/rapport_phase_scanning.routes.js
+++ routes/administration/rapport_phase_scanning.routes.js
@@ -3,9 +3,7 @@ const express = require('express')
 const rapport_phase_scanning_controler=require('../../controllers/administration/rapport/phase_scanning.controller')
 const rapport_phase_scanning_routes=express.Router()
 
-rapport_phase_scanning_routes.get('/dossier_scanne_non_scanner/:ID_FOLIO_EQUIPE',rapport_phase_scanning_controler.dossiers_scannes_non_scannees)
-
-rapport_phase_scanning_routes.get('/allequipe',rapport_phase_scanning_controler.equipesAll)
+rapport_phase_scanning_routes.get('/phasescanning',rapport_phase_scanning_controler.phaseScanning)
 
 module.exports = rapport_phase_scanning_routes
 
