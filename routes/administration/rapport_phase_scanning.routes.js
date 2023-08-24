24ee1f9363b34e0dcb6c39278ebf50eb426466bd
habiyakareleonard <habiyakareleonard2019@gmail.com>
2023-08-24 17:37:33 +0200

    rapport combine

--- routes/administration/rapport_phase_scanning.routes.js
+++ routes/administration/rapport_phase_scanning.routes.js
@@ -4,7 +4,10 @@ const rapport_phase_scanning_controler=require('../../controllers/administration
 const rapport_phase_scanning_routes=express.Router()
 
 rapport_phase_scanning_routes.get('/phasescanning',rapport_phase_scanning_controler.phaseScanning)
-
+rapport_phase_scanning_routes.get('/agentdesarchivages',rapport_phase_scanning_controler.rapport_agent_desarchivage)
+rapport_phase_scanning_routes.get('/agentsuperviseur',rapport_phase_scanning_controler.agent_superviseur)
+rapport_phase_scanning_routes.get('/chefplateau',rapport_phase_scanning_controler.agent_chefplateau)
+rapport_phase_scanning_routes.get('/chefequipe',rapport_phase_scanning_controler.agent_chefequipe)
 module.exports = rapport_phase_scanning_routes
 
 
